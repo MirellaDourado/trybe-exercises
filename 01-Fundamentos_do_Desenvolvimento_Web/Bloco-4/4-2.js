98bd4373e60603146d704f8e22173e329d86a66e
MirellaDourado <mirellaalvesdourado@gmail.com>
2022-07-21 18:33:49 -0300

    questões 3 e 4

--- 01-Fundamentos_do_Desenvolvimento_Web/Bloco-4/4-2.js
+++ 01-Fundamentos_do_Desenvolvimento_Web/Bloco-4/4-2.js
@@ -1,14 +1,38 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let resultadoSomaArray = null;
 
-// //  Percorra o array
+// 1.  Percorra o array
 // for(let percorrendoArray of numbers){
 //   console.log(percorrendoArray)
 // }
 
 
-//  Some os valores do array
-for (let somaArray = 0; somaArray < (numbers.length -1); somaArray+= 1){
-  let resultadoSoma = numbers[somaArray] + numbers[somaArray + 1];
-  console.log(resultadoSoma)
-};
+// 2 . Some os valores do array
+//  for (let somaArray = 0; somaArray < (numbers.length -1); somaArray += 1){
+//   let resultadoSoma = numbers[somaArray] + numbers[somaArray + 1];
+//   console.log(resultadoSoma)
+// };
+
+
+// 3. Calcule e imprima a média aritmética dos valores 
+// let somatorio = null;
+
+// for(let index = 0; index < (numbers.length); index += 1){
+//   somatorio+=numbers[index];
+// };
+
+// console.log(somatorio/numbers.length)
+
+
+// 4. Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20"
+// const mediaAritmetica = somatorio/numbers.length
+
+// if(mediaAritmetica > 20){
+//   console.log("Valor maior que 20")
+// } else {
+//   console.log("Valor menor ou igual a 20")
+// }
+
+
+// 5. Com o for, descubra qual o maior valor contido no array e imprima-o
+
