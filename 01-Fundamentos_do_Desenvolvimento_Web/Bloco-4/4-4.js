e6df2c6be3bab3dc5365cdacc8eb32acce634c2a
MirellaDourado <mirellaalvesdourado@gmail.com>
2022-07-25 18:31:42 -0300

    Questão 6

--- 01-Fundamentos_do_Desenvolvimento_Web/Bloco-4/4-4.js
+++ 01-Fundamentos_do_Desenvolvimento_Web/Bloco-4/4-4.js
@@ -43,3 +43,20 @@ if (info.recorrente !== infoCharacter.recorrente){
 } else {
   console.log('Ambos recorrentes.')
 }
+
+// 6. Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'"
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+console.log('O livro favorito de', leitor.nome, leitor.sobrenome, 'se chama', leitor.livrosFavoritos[0].titulo)
