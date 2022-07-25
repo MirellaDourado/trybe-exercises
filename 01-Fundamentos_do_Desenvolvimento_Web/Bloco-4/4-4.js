e71501d5d9bbdb20ab1fa95c6b4360c53b391174
MirellaDourado <mirellaalvesdourado@gmail.com>
2022-07-25 18:28:02 -0300

    Questões 3, 4 e 5

--- 01-Fundamentos_do_Desenvolvimento_Web/Bloco-4/4-4.js
+++ 01-Fundamentos_do_Desenvolvimento_Web/Bloco-4/4-4.js
@@ -15,3 +15,31 @@ info.recorrente = 'Sim'
 console.log(info)
 
 //  3. Faça um for/in que mostre todas as chaves do objeto
+
+for(let key in info){
+  console.log(key)
+}
+
+// 4. Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto
+for(let key in info){
+  console.log(info[key])
+}
+
+// 5.
+
+let infoCharacter = {
+  personagem: 'Tio Patinhas', 
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas', 
+  recorrente:'Sim'
+}
+
+console.log(info.personagem, 'e', infoCharacter.personagem)
+console.log(info.origem, 'e', infoCharacter.origem)
+console.log(info.nota, 'e', infoCharacter.nota)
+
+if (info.recorrente !== infoCharacter.recorrente){
+  console.log(info.recorrente, 'e', infoCharacter.recorrente)
+} else {
+  console.log('Ambos recorrentes.')
+}
