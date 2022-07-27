0c1cae3754cacfc46d07ce442c4bd29abc79163e
MirellaDourado <mirellaalvesdourado@gmail.com>
2022-07-27 18:29:13 -0300

    exercicios 3

--- 01-Fundamentos_do_Desenvolvimento_Web/Bloco-5/5-1/script.js
+++ 01-Fundamentos_do_Desenvolvimento_Web/Bloco-5/5-1/script.js
@@ -4,7 +4,6 @@
         - document.getElementsByClassName()
         - document.getElementsByTagName()
         . (Não gaste tempo pensando no texto e sim realizando o exercício)
-        2. Crie e execute uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
         3. Crie e execute uma função que mude a cor do quadrado vermelho para branco.
         4. Crie e execute uma função que corrija o texto da tag <h1>.
         5. Crie e execute uma função que modifique todo o texto da tag <p> para maiúsculo.
@@ -18,3 +17,7 @@ document.getElementsByTagName('p')[1].innerText = 'lorem ipsum';
 // 2. Crie e execute uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
 
 document.getElementsByClassName('main-content')[0].style = 'background-color: rgb(76,164,109)'
+
+// 3. Crie e execute uma função que mude a cor do quadrado vermelho para branco.
+
+document.getElementsByClassName('center-content')[0].style = 'background-color: white'
