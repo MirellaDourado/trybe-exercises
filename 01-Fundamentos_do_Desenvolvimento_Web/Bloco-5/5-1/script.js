93538418f4c9cf4a9e542cd2f907ddc70601b489
MirellaDourado <mirellaalvesdourado@gmail.com>
2022-07-28 18:10:40 -0300

    implementação das funções

--- 01-Fundamentos_do_Desenvolvimento_Web/Bloco-5/5-1/script.js
+++ 01-Fundamentos_do_Desenvolvimento_Web/Bloco-5/5-1/script.js
@@ -9,26 +9,52 @@
         
 
 // 1. Crie e execute uma função que mude o texto na tag `<p>-----</p>`, para uma descrição de como você se vê daqui a 2 anos
-
-  document.getElementsByTagName('p')[1].innerText = 'lorem ipsum';
+  function mudarParágrafo() {
+    let paragrafo = document.getElementsByTagName('p')[1];
+    paragrafo.innerText = 'Codando muito em uma empresa que eu posa fazer a diferença.';
+  }
+  mudarParágrafo();
+  
 
 // 2. Crie e execute uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
+  function corTrybe () {
+    let mainContent = document.getElementsByClassName('main-content')[0].style.backgroundColor ='rgb(76,164,109)';
+    return mainContent
+  }
+  corTrybe();
 
-  document.getElementsByClassName('main-content')[0].style = 'background-color: rgb(76,164,109)'
 
 // 3. Crie e execute uma função que mude a cor do quadrado vermelho para branco.
+  function quadradoBranco () {
+    let quadrado = document.getElementsByClassName('center-content')
+    quadrado[0].style = 'background-color: white'
+  }
 
+  quadradoBranco();
   document.getElementsByClassName('center-content')[0].style = 'background-color: white'
 
 // 4. Crie e execute uma função que corrija o texto da tag <h1>.
-  document.getElementsByTagName('h1')[0].innerText = 'Exercício 5.1 - JavaScript'
-
+  function corrigirH1() {
+    let tituloErrado = document.getElementsByTagName('h1')[0];
+    tituloErrado.innerText = 'Exercício 5.1 - JavaScript'
+  }
+  corrigirH1();
+  
 // 5. Crie e execute uma função que modifique todo o texto da tag <p> para maiúsculo.
-  for(let index = 0; index < document.getElementsByTagName('p').length; index += 1){
-    document.getElementsByTagName('p')[index].style = 'text-transform: uppercase'
-  } 
+  function capsLock() {
+    let paragrafoCapsLock= document.getElementsByTagName('p');
+    for(let index = 0; index < paragrafoCapsLock.length; index += 1){ 
+      paragrafoCapsLock[index].style = 'text-transform: uppercase'
+    } 
+  }
+  capsLock();
+  
   
 // 6. Crie e execute uma função que exiba o conteúdo de todas as tags <p> no console.
-  for(let index = 0; index < document.getElementsByTagName('p').length; index += 1){
-    console.log(document.getElementsByTagName('p')[index].innerText)
-  }
+  function paragrafosNoConsole() {
+    for(let index = 0; index < document.getElementsByTagName('p').length; index += 1) {
+      console.log(document.getElementsByTagName('p')[index].innerText)
+    }
+  }  
+
+  paragrafosNoConsole();
