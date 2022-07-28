bf86e5e68e911a764dd9cd8f099cf475187b776e
MirellaDourado <mirellaalvesdourado@gmail.com>
2022-07-28 20:02:59 -0300

    exercicio 1

---
+++ 01-Fundamentos_do_Desenvolvimento_Web/Bloco-5/5-2/script.js
@@ -0,0 +1,5 @@
+// 1. 🚀 Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body;
+let tituloPrincipal = document.createElement('h1')
+tituloPrincipal.innerHTML = 'Exercício 5.2 - JavaScript DOM'
+document.body.appendChild(tituloPrincipal)
+
