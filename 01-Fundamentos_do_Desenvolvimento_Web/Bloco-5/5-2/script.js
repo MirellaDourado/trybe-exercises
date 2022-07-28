78500c14513a7a465a5e1be307e84e5d3e434ab3
MirellaDourado <mirellaalvesdourado@gmail.com>
2022-07-28 20:03:42 -0300

    exercicio 2

--- 01-Fundamentos_do_Desenvolvimento_Web/Bloco-5/5-2/script.js
+++ 01-Fundamentos_do_Desenvolvimento_Web/Bloco-5/5-2/script.js
@@ -3,3 +3,7 @@ let tituloPrincipal = document.createElement('h1')
 tituloPrincipal.innerHTML = 'Exercício 5.2 - JavaScript DOM'
 document.body.appendChild(tituloPrincipal)
 
+// 2. 🚀 Adicione a tag main com a classe main-content como filho da tag body;
+let mainContent = document.createElement('main')
+document.body.appendChild(mainContent)
+
