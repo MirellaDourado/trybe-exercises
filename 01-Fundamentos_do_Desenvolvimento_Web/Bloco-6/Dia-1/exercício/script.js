d5b8a62b1b2483e2073db08c5ddf2c8558f8e704
MirellaDourado <mirellaalvesdourado@gmail.com>
2022-08-05 18:54:10 -0300

    quesito 3

---
+++ 01-Fundamentos_do_Desenvolvimento_Web/Bloco-6/Dia-1/exercício/script.js
@@ -0,0 +1,7 @@
+let submitBtn = document.querySelector('#submit-btn');
+
+function buttonSubmit(targetBtn) {
+  targetBtn.preventDefault(targetBtn)
+}
+
+submitBtn.addEventListener('click',buttonSubmit)
