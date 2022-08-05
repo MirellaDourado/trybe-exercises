4a1eeb6c6475d02401667836e2a327bd9264f279
MirellaDourado <mirellaalvesdourado@gmail.com>
2022-08-05 19:27:55 -0300

    bonus

--- 01-Fundamentos_do_Desenvolvimento_Web/Bloco-6/Dia-1/exercício/script.js
+++ 01-Fundamentos_do_Desenvolvimento_Web/Bloco-6/Dia-1/exercício/script.js
@@ -1,7 +1,17 @@
-let submitBtn = document.querySelector('#submit-btn');
+const submitBtn = document.querySelector('#submit-btn');
+const photosRights = document.querySelector('#rights')
+const photos = document.querySelector('#photos')
 
 function buttonSubmit(targetBtn) {
   targetBtn.preventDefault(targetBtn)
 }
-
 submitBtn.addEventListener('click',buttonSubmit)
+
+function filesSub() {
+  if (photosRights.checked){
+    photos.style.display = 'block'
+  } else {
+    photos.style.display = 'none'
+  }
+}
+photosRights.addEventListener('click', filesSub)
