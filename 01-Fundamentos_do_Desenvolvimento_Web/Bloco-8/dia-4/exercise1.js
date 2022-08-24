556c5b595b895b0f43941266c304c0a61fed1712
MirellaDourado <mirellaalvesdourado@gmail.com>
2022-08-24 18:30:49 -0300

    refatorado exercise 1 - bloco 8 - dia 4

--- 01-Fundamentos_do_Desenvolvimento_Web/Bloco-8/dia-4/exercise1.js
+++ 01-Fundamentos_do_Desenvolvimento_Web/Bloco-8/dia-4/exercise1.js
@@ -6,10 +6,6 @@ const arrays = [
   [4, 5, 6],
 ];
 
-function flatten() {
-  return arrays.reduce( (acc, curr) => {
-    return acc.concat(curr)
-  }, [])
-}
+const flatten = () => arrays.reduce( (acc, curr) => acc.concat(curr), []);
 
 console.log(flatten())
