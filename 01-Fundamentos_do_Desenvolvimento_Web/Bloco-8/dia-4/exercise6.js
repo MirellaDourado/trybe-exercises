1a6e7440ca0404f024d5b208ef834544ac23a4e6
MirellaDourado <mirellaalvesdourado@gmail.com>
2022-08-24 20:18:59 -0300

    exercise 6 - bloco 8 - dia 4

---
+++ 01-Fundamentos_do_Desenvolvimento_Web/Bloco-8/dia-4/exercise6.js
@@ -0,0 +1,26 @@
+// 🚀 6- Para o próximo exercício você deve criar um novo array de objetos a partir das informações das constantes students e grades, onde cada objeto desse novo array terá o formato { name: nome do aluno, average: media das notas }. Para isso vamos assumir que a posição do index 0 de grades refere-se ao estudante na posição de index 0 de students. Para resolver este exercício, além de reduce, será necessário utilizar também a função map. Dica: Você pode acessar o index do array dentro de map, e você pode ver o objeto esperado na constante expected.
+
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+function studentAverage() {
+  const average = grades.map( (grade) =>{
+    return grade.reduce( (acc, acurr) => {
+      return acc + acurr
+    }) / grade.length
+  })
+
+  return students.map( (student, index) => {
+    return {name: student, average: average[index]}
+  },)
+
+
+}
+
+console.log(studentAverage())
+
+const expected = [
+  { name: 'Pedro Henrique', average: 7.8 },
+  { name: 'Miguel', average: 9.2 },
+  { name: 'Maria Clara', average: 8.8 },
+];
