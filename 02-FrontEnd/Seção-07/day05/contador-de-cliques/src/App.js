e5ffe671ba24ba1b84d037e02c5f4a5424b095ab
MirellaDourado <mirellaalvesdourado@gmail.com>
2022-10-28 16:21:56 -0300

    implementação do test do contador de cliques

--- 02-FrontEnd/Seção-07/day05/contador-de-cliques/src/App.js
+++ 02-FrontEnd/Seção-07/day05/contador-de-cliques/src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import ButtonClicks from './ButtonClicks';
 import NumberClicks from './NumberClicks';
-import store from './store';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Provider store={store}>
           <ButtonClicks />
           <NumberClicks />
-        </Provider>
       </div>
     );
   }
--- 02-FrontEnd/Seção-07/day05/contador-de-cliques/src/App.test.js
+++ 02-FrontEnd/Seção-07/day05/contador-de-cliques/src/App.test.js
@@ -1,8 +1,34 @@
-import { render, screen } from '@testing-library/react';
+import { cleanup, render, screen } from '@testing-library/react';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { combineReducers, legacy_createStore as createStore } from 'redux';
 import App from './App';
+import clickReducer from './reducers';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const renderWithRedux = (
+  component,
+  {
+    initialState,
+    store = createStore(combineReducers({ clickReducer }), initialState),
+  } = {}
+) => ({
+  ...render(<Provider store={store}>{component}</Provider>),
+  store,
+});
+
+describe('testing clicks', () => {
+  beforeEach(cleanup);
+  test('the page should have a button and a text 0', () => {
+    renderWithRedux(<App />);
+    const buttonAdicionar = screen.queryByText('Clique aqui');
+
+    expect(buttonAdicionar).toBeInTheDocument();
+    expect(screen.getByText('0')).toBeInTheDocument();
+  });
+
+  test('a click in a button should increment the value of clicks', () => {
+    renderWithRedux(<App />, { initialState: { clickReducer: { counter: 5 } } });
+
+    expect(screen.getByText('5')).toBeInTheDocument();
+  });
 });
